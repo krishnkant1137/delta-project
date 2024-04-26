bd80b12c096d0c1c31a2944d85baadbb5a12a043
krishnkant1137 <120434528+krishnkant1137@users.noreply.github.com>
2024-04-26 07:57:09 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,44 +1,41 @@
-const User = require("../models/user");
-
-module.exports.renderSignupForm = (req, res) => {
-  res.render("users/signup.ejs");
-}
+const User = require('../models/user.js');
 
 module.exports.signup = async (req, res) => {
-  try {
-   let {username, email, password} = req.body;
-   const newUser = new User({email, username});
-   const registeredUser = await User.register(newUser, password);
-   console.log(registeredUser);
-   req.login(registeredUser, (err) => {
-     if(err) {
-       return next(err);
-     }
-     req.flash("success", "Welcome to AirBnb");
-     res.redirect("/listings");
-   })
-  } catch(e) {
-   req.flash("error", e.message);
-   res.redirect("/signup");
-  }
- };
-
- module.exports.renderLoginForm = (req, res) => {
-  res.render("users/login.ejs");
-}
-
-module.exports.login = async(req, res) => {
-  req.flash("success", "Welcome back to AirBnb! You are logged in!");
-  let redirectUrl = res.locals.redirectUrl || "/listings";
-  res.redirect(redirectUrl);
-}
-
-module.exports.logout = (req, res) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err);
+    try {
+        const { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registerUser = await User.register(newUser, password);
+        req.login(registerUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to WanderLust");
+            res.redirect("/listings");
+        })
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect('/signup');
     }
-    req.flash("success", "you are logged out!");
-    res.redirect("/listings");
-  })
+}
+
+module.exports.login = async (req, res) => {
+    req.flash("success", "Welcome back to WanderLust");
+    //when we will try to login form "/listings" then isLoggedIn middleware will never be called and req.session.redirectUrl will remain uninitilized ie null.
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+
+    if (redirectUrl.includes("DELETE")) {
+        redirectUrl = "/listings";
+    }
+
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", " You are logged out now");
+        res.redirect("/listings");
+    });
 }

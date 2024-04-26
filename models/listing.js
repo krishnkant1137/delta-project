8dd7aa80795a3fd696ea1e9f0a0f2cbccb67309e
krishnkant1137 <120434528+krishnkant1137@users.noreply.github.com>
2024-04-26 08:01:27 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,49 +1,49 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const review = require("./review.js");
-const { required } = require("joi");
+const Review = require('./review.js');
 
 const listingSchema = new Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: String,
-  image: {
-    url: String,
-    filename: String,
-  },
-  price: Number,
-  location: String,
-  country: String,
-  reviews: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Review",
+    title: {
+        type: 'string',
+        required: true
     },
-  ],
-  owner: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-  },
-  geometry: {
-    type: {
-    type: String,
-    enum: ["Point"],
-    required: true,
-  },
-  coordinates: {
-    type: [Number],
-    required: true,
-  },
-},
+    description: String,
+    image: {
+        url: String,
+        filename: String
+    },
+    price: Number,
+    location: String,
+    country: String,
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        }
+    ],
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+    geometry: {
+        type: {
+            type: String, // Don't do `{ location: { type: String } }`
+            enum: ['Point'], // 'location.type' must be 'Point'
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
+        }
+    }
 });
 
+//Post Middleware which will executed after execution of findOneAndDelete
 listingSchema.post("findOneAndDelete", async (listing) => {
-  if(listing) {
-    await review.deleteMany({_id : {$in: listing.reviews}});
-  }
+    if (listing) {
+        await Review.deleteMany({ _id: { $in: listing.reviews } });
+    }
 });
 
-const Listing = mongoose.model("listing", listingSchema);
+const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;

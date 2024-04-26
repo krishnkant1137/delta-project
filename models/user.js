bb9a2a6772bb4e3e050ed97b73c8db7069caf38b
krishnkant1137 <120434528+krishnkant1137@users.noreply.github.com>
2024-04-26 08:02:52 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
-const { required } = require("joi");
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
 const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true
-  }
+    email: {
+        type: 'string',
+        required: true
+    }
+    // Passport-Local Mongoose will add a username, hash and salt field to store the username, the hashed password and the salt value.
 });
 
 userSchema.plugin(passportLocalMongoose);

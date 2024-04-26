ea2672b66449afbb8dddddab24998c86202b4e27
krishnkant1137 <120434528+krishnkant1137@users.noreply.github.com>
2024-04-26 20:13:01 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -2,13 +2,13 @@ mapboxgl.accessToken = mapToken;
 
 const map = new mapboxgl.Map({
     container: 'map', // container ID
-    center: listing.geometry.coordinates, // starting position [lng, lat]
+    center: [77.2090, 28.6139], // starting position [lng, lat]
     zoom: 9 // starting zoom
 });
 
-// console.log("working");
+console.log("working");
 // console.log(listing.geometry.coordinates);
 
-const marker = new mapboxgl.Marker({ color: 'red', rotation: 45 })
-    .setLngLat(listing.geometry.coordinates)
-    .addTo(map);
+// const marker = new mapboxgl.Marker({ color: 'black', rotation: 45 })
+//     .setLngLat([77.2090, 28.6139])
+//     .addTo(map);

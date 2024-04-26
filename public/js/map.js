8b08a7a0b1fe0925a435dc386e8efa2ccdbe4a10
krishnkant1137 <120434528+krishnkant1137@users.noreply.github.com>
2024-04-26 17:42:11 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,18 +1,14 @@
-	mapboxgl.accessToken = mapToken;
+mapboxgl.accessToken = mapToken;
 
-  const map = new mapboxgl.Map({
-      container: 'map', // container ID
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: listing.geometry.coordinates, // starting position [lng, lat]
-      zoom: 9 // starting zoom
-  });
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    center: listing.geometry.coordinates, // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
 
+// console.log("working");
+// console.log(listing.geometry.coordinates);
 
-  const marker = new mapboxgl.Marker({color: "red"})
-  .setLngLat(listing.geometry.coordinates) // Listing.geometry.coordinates
-  .setPopup(
-    new mapboxgl.Popup({ offset: 25 }).setHTML(
-      `<h4>${listing.title}</h4><p>Exact Location will be provided after booking</p>`
-    )
-  )
-  .addTo(map);
+const marker = new mapboxgl.Marker({ color: 'red', rotation: 45 })
+    .setLngLat(listing.geometry.coordinates)
+    .addTo(map);

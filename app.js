c6d40433f7f610a9b3fcc2315cfc07e18fda6cf8
krishnkant1137 <120434528+krishnkant1137@users.noreply.github.com>
2024-04-26 16:23:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -110,7 +110,7 @@ app.all("*", (req, res, next) => {
 //Error handling 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something went wrong" } = err;
-    res.status(statusCode).render("./listing/error.ejs", { err })
+    res.status(statusCode).render("./error.ejs", { err })
     // res.status(statusCode).send(message);
 })
 

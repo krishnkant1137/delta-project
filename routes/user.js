5671b659116d059446f9639962cc9106778d9c74
krishnkant1137 <120434528+krishnkant1137@users.noreply.github.com>
2024-04-26 08:14:47 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,32 @@
-const express = require("express");
-const router = express.Router();
-const User = require("../models/user");
-const wrapAsync = require("../utils/wrapAsync");
-const passport = require("passport");
-const { saveRedirectUrl } = require("../middleware.js");
 
-const userController = require("../controllers/users.js");
+const express = require('express');
+const router = express.Router({ mergeParams: true });
+const wrapAsync = require('../utils/wrapAsync');
+const passport = require('passport');
+const { saveRedirectUrl } = require("../middleware.js")
+const userController = require('../controllers/users.js')
 
-router
-.route("/signup")
-.get( userController.renderSignupForm)
-.post( wrapAsync(userController.signup));
+//Signup routes
+router.route('/signup')
+    .get((req, res) => {
+        res.render("./users/signup.ejs");
+    })
+    .post(wrapAsync(userController.signup));
 
-router
-.route("/login")
-.get( userController.renderLoginForm)
-.post( 
-  saveRedirectUrl,
-  passport.authenticate("local", {
-     failureRedirect: `/login`,
-      failureFlash: true,
-     }), 
-     userController.login);
+//Login routes
+router.route('/login')
+    .get((req, res) => {
+        res.render("./users/login.ejs");
+    })
+    .post(
+        saveRedirectUrl,
+        passport.authenticate('local', { failureRedirect: '/login', failureFlash: true }),
+        wrapAsync(userController.login)
+    );
 
-router.get("/logout", userController.logout)
+
+//LogOut routes
+//Passport has inbuild function for logout
+router.get('/logout', userController.logout);
 
 module.exports = router;
